4c4539ee09c255845e5effb09d329dcc31dd8f86
IgorT1 <106947961+IgorT1@users.noreply.github.com>
2023-04-04 07:27:42 +0300

    Update AFINI.cpp

--- AFINI/AFINI.cpp
+++ AFINI/AFINI.cpp
@@ -1,9 +1,9 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-int modInverse(int a, int m) {
+int modInverse(int a, int m) {//метод для нахождение числа по модулю
     a = a % m;
     for (int x = 1; x < m; x++) {
         if ((a * x) % m == 1) {
@@ -13,16 +13,15 @@ int modInverse(int a, int m) {
     return 1;
 }
 
-string affineDecrypt(string ciphertext, int a, int b) {
-    string plaintext = "";
-    int a_inv = modInverse(a, 26);
+string affineDecrypt(string ciphertext, int a, int b) {// метод для расшифровки 
+    string plaintext = "";//строка для результата
+    int a_inv = modInverse(a, 26);// передача значений в метод 
     for (char c : ciphertext) {
-        if (c >= 'a' && c <= 'z') {  
-            int x = (a_inv * (c - 'a' - b + 26)) % 26; 
+        if (c >= 'a' && c <= 'z') {  //проверка на соответсвие англ алфавита 
+            int x = (a_inv * (c - 'a' - b + 26)) % 26; //формула для расшифровки 
             plaintext += (char)(x + 'a');
         }
         else {
-            tolower(c); 
             plaintext += c;
         }
     }
@@ -34,9 +33,9 @@ int main() {
     string ciphertext = "jzq kuudncrejqg ut jzq jedwai jus tumd qlqpqr";
     string target = "target";
 
-    for (int a = 3; a < 5; a++) { // êëþ÷è ïîë áàéêà (4)
+    for (int a = 3; a < 5; a++) { // размер ключа пол байта (4)
         int a_inv = modInverse(a, 26);
-        for (int b = 3; b < 5; b++) {//òàêæå 
+        for (int b = 3; b < 5; b++) {//аналогичнор 
             string plaintext = affineDecrypt(ciphertext, a, b);
             cout << "ключи : a = " << a << ", b = " << b << endl;
             cout << "результат: " << plaintext << endl;
